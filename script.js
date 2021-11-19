6419eaa8c43c8563829751aa3c8d5b1e97e5363a
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2021-11-19 21:53:01 +0500

    lamp

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("Btn");
+var light = document.getElementById("light");
+function toggleBtn(){
+    btn.classList.toggle('active');
+    light.classList.toggle('on');
+}
